b6b3fb87f4651a6519f4158ceffdadfa316f6ea4
Zoey Wen <zwen@sbeck-iMac.local>
2019-10-11 10:27:50 -0400

    First commit

---
+++ src/build/precache-manifest.4579f2db3845c58f5825da303728f28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009c49c00e8b1ff4a584c711bb476ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11301ec1cb73b81be2b0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76419f3fe34ed0826b7a",
+    "url": "/static/js/2.55ce816f.chunk.js"
+  },
+  {
+    "revision": "11301ec1cb73b81be2b0",
+    "url": "/static/js/main.63a48179.chunk.js"
+  },
+  {
+    "revision": "5d3df65c6aad5af6f7eb",
+    "url": "/static/js/runtime-main.8a83d180.js"
+  }
+]);
